4c69666805822d9df2fc84835a5d14f0ba66f032
budickda <daniel@budick.eu>
2015-09-03 20:09:42 +0200

    Reader.update() updates text in the view.

--- reader.js
+++ reader.js
@@ -1,1 +1,37 @@
 Reader = {};
+
+Reader.update = function () {
+    if (!Spielebuch) {
+        console.error(500, 'Please install package spielebuch:core by typing \'meteor add spielebuch:core\' into your console.');
+        return false;
+    }
+    Session.set('text', Reader.parseKeywordText(Spielebuch.getText()));
+    return Reader.parseKeywordText(Spielebuch.getText());
+};
+
+Reader.parseKeywordText = function (textarray) {
+    var re = /[^[\]]+(?=])/, _ids = [];
+    if (Array.isArray(textarray)) {
+        _.map(textarray, function (text) {
+            _ids = re.exec(text);
+            console.log(text);
+            console.log(_ids);
+            _.forEach(_ids, function (_id) {
+                var gameobject = Spielebuch.Gameobjects.findOne(_id);
+                if (!gameobject) {
+                    console.error('404', 'Could not find Gameobject ('+_id+') in database.');
+                    return;
+                }
+                text = text.replace(new RegExp('\\[' + _id + '\\]', 'g'),
+                    '<a href=\"#\" class=\"keyword hover\" ' +
+                    'data-_id=\"' + _id + '\">' + gameobject.name +
+                    '</a>'
+                );
+            });
+            return text + ' ';
+        });
+        return textarray;
+    }
+    console.error(500, 'Spielebuch.getText() should return an array, but instead returned a ' + typeof textarray + '.');
+    return [];
+};
--- view/story.js
+++ view/story.js
@@ -1,26 +1,18 @@
-var parseKeywordText = function(text){
-    var re = /[^[\]]+(?=])/, _ids = re.exec(text);
 
-    _.forEach(_ids, function(_id){
-        var gameobject = Gameobjects.findOne(_id);
-        text.replace(new RegExp('\\[' + _id + '\\]', 'g'),
-            '<a href=\"#\" class=\"keyword hover\" ' +
-            'data-_id=\"' + _id + '\">' + gameobject.name +
-            '</a>'
-        );
-    });
-
-    return text;
-};
 
 Template.story.events({
 });
 
 Template.story.helpers({
     story: function(){
-        return parseKeywordText(Session.get('storytext'));
-    },
-    loggedIn: function() {
-        return !!Meteor.userId();
+        if(Session.get('playingScene')===-1) {
+            return ['Loading...'];
+        }
+        if(Session.get('playingScene')===undefined){
+            return ['Please install package spielebuch:core.',
+                '<br/>To do this type: \'meteor add spielebuch:core\' into your console.'];
+        }
+        Reader.update();
+        return Session.get('text');
     }
 });
